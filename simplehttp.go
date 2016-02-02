db3b1d3cbf205b80046ea41405c845de176e603e
Markus Becker <mtb@tibyte.net>
2016-02-02 15:11:40 +0100

    Testing

--- simplehttp.go
+++ simplehttp.go
@@ -1,17 +1,52 @@
 package simplehttp
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
+// Identity contains information about the network id
+type Identity struct {
+	Hostname, ExternalIP string
+}
+
+func (i Identity) String() string {
+	return fmt.Sprintf("%s (%s)", i.Hostname, i.ExternalIP)
+}
+
 // GetResponeBody return the resp.Body() string using GET
-func GetResponeBody(url string) (string, error) {
+func GetResponeBody(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	return string(body), err
+	return body, err
+}
+
+// GetExternalIP returns the external ip4 or ip6 adress using httpbin.org
+func GetExternalIP() (string, error) {
+	ipjson, err := GetResponeBody("http://httpbin.org/ip")
+	if err != nil {
+		return "127.0.0.1", err
+	}
+	var ipresp struct {
+		Origin string `json:"origin"`
+	}
+	err = json.Unmarshal(ipjson, &ipresp)
+	return ipresp.Origin, err
+}
+
+// GetWhoAmI in the form HOSTNAME (EXTERNAL-IP)
+func GetWhoAmI() (Identity, error) {
+	h, err := os.Hostname()
+	if err != nil {
+		return Identity{"localhost", "127.0.0.1"}, err
+	}
+	i, err := GetExternalIP()
+	return Identity{h, i}, err
 }
--- simplehttp_test.go
+++ simplehttp_test.go
@@ -2,13 +2,42 @@ package simplehttp
 
 import (
 	"fmt"
+	_ "os"
 	"testing"
 )
 
 func TestHTTP(t *testing.T) {
 	jsresp, err := GetResponeBody("http://httpbin.org/ip")
 	if err != nil {
+		fmt.Print(err)
 		t.FailNow()
 	}
-	fmt.Printf("IP-Json:\n%v", jsresp)
+	fmt.Printf("IP-Json:\n%v", string(jsresp))
+}
+
+func TestExternalIP(t *testing.T) {
+	ext, err := GetExternalIP()
+	if err != nil {
+		fmt.Print(err)
+		t.FailNow()
+	}
+	fmt.Printf("IP-Adress: %v\n", ext)
+}
+
+func TestWAI(t *testing.T) {
+	hn, err := GetWhoAmI()
+	if err != nil {
+		fmt.Print(err)
+		t.FailNow()
+	}
+	fmt.Printf("I am: %v\n", hn)
+}
+
+func TestIdentity(t *testing.T) {
+	var id Identity
+	id = Identity{"localhost", "127.0.0.1"}
+	if "simplehttp.Identity: localhost (127.0.0.1)" != fmt.Sprintf("%T: %v", id, id) {
+		fmt.Print(fmt.Sprintf("%T: %v", id, id))
+		t.FailNow()
+	}
 }
